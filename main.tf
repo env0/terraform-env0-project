5883b370ceb7a371764cc20c1fe4194c8bd1fbef
Andrew Way <andrew.way@env0.com>
2021-12-10 13:45:33 -0800

    add credential support

--- main.tf
+++ main.tf
@@ -11,12 +11,14 @@ terraform {
 provider "env0" {
 }
 
+# create the project with the name and description
 resource "env0_project" "project" {
   for_each    = var.projects
   name        = each.value.name
   description = each.value.description
 }
 
+# configure the policy for each project
 resource "env0_project_policy" "policy" {
   for_each                      = var.projects
   project_id                    = env0_project.project[each.key].id
@@ -31,11 +33,32 @@ resource "env0_project_policy" "policy" {
   skip_redundant_deployments    = each.value.policy.skip_redundant_deployments
 }
 
+# using the data resource to get the credential ID
+data "env0_aws_credentials" "credentials" {
+    for_each = var.projects
+    name = each.value.credential
+}
+
+# configure the project with the proper credentials
+resource "env0_cloud_credentials_project_assignment" "credential_project" {
+  for_each = var.projects
+  project_id    = env0_project.project[each.key].id
+  credential_id = data.env0_aws_credentials.credentials[each.key].id
+}
+
+###
+#  projects variable split into four main fields
+#    * name (string) - the name of the project as shown in the UI
+#    * description (string) - the description of the project as shown in the UI
+#    * credential (string) - the name of the credential (AWS Assume Role) assigned to the Project
+#    * policy (object) - the project policy settings
+###
 variable "projects" {
   type = map(
     object({
       name        = string
       description = string
+      credential  = string
       policy = object({
         continuous_deployment_default = optional(bool)
         disable_destroy_environments  = bool
@@ -49,37 +72,8 @@ variable "projects" {
       })
     })
   )
-
-  description = "map of object with names and descriptions e.g. {dev0={name=\"dev\",description=\"this is my dev project\",policy=local.devpolicy}}"
+  description = "map of object with names, descriptions, credentials and policies e.g. {dev0={name=\"dev\",description=\"this is my dev project\",credential=\"aws dev\",policy=local.devpolicy}}"
 }
-
-# variable "policy" {
-#   type = object({
-#     continuous_deployment_default = optional(bool)
-#     disable_destroy_environments  = bool
-#     include_cost_estimation       = bool
-#     number_of_environments        = string
-#     number_of_environments_total  = string
-#     requires_approval_default     = bool
-#     run_pull_request_plan_default = bool
-#     skip_apply_when_plan_is_empty = bool
-#     skip_redundant_deployments    = bool
-#   })
-
-#   description = "project policy"
-
-#   default = {
-# continuous_deployment_default = true
-# disable_destroy_environments  = false
-# include_cost_estimation       = true
-# number_of_environments        = "3"
-# number_of_environments_total  = "10"
-# requires_approval_default     = true
-# run_pull_request_plan_default = true
-# skip_apply_when_plan_is_empty = true
-# skip_redundant_deployments    = true
-#   }
-# }
 
 output "id" {
   value = {
