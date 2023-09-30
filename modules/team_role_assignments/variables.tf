413b073372ae2d8a412f571f6c6c48c5ac359051
Andrew Way <andrew.way@env0.com>
2023-09-29 22:14:06 -0700

    add support for standard roles

--- modules/team_role_assignments/variables.tf
+++ modules/team_role_assignments/variables.tf
@@ -2,7 +2,8 @@ variable "team_role_assignments" {
   type = list(
     object({
       team_name        = string
-      custom_role_name = string
+      role             = optional(string)
+      custom_role_name = optional(string)
   }))
   description = "mapping of team name to custom role name"
 }
